a2d60cf96691f8494ae10ebe323f95e82366f4d4
Rizqy <rizqy.dev@gmail.com>
2026-02-19 11:17:20 +0700

    learn singleton

---
+++ src/main/java/rizqy/dev/demo/Database.java
@@ -0,0 +1,16 @@
+package rizqy.dev.demo;
+
+public class Database {
+
+  private static Database database;
+
+  public static Database getInstance() {
+    if (database == null) {
+      database = new Database();
+    }
+
+    return database;
+  }
+
+  private Database() {}
+}
